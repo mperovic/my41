11480d0363071af9f4be4829f20c8e5994a47281
Miroslav Perovic <miroslav@appricot.com>
2015-01-09 10:45:45 +0100

    v1.0

---
+++ my41CX.ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my41cx.ios
+//
+//  Created by Miroslav Perovic on 1/8/15.
+//  Copyright (c) 2015 iPera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
