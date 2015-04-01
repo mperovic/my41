71ce7033e16f349e598c0f0791fdc30e4da2a80f
Miroslav Perovic <miroslav@appricot.com>
2015-04-01 08:49:50 +0200

    App Store Final

--- my41/Classes/Extensions.swift
+++ my41/Classes/Extensions.swift
@@ -8,16 +8,13 @@
 
 import Foundation
 
-extension String
-	{
-	subscript(integerIndex: Int) -> Character
-		{
+extension String {
+	subscript(integerIndex: Int) -> Character {
 		let index = advance(startIndex, integerIndex)
 			return self[index]
 	}
 	
-	subscript(integerRange: Range<Int>) -> String
-		{
+	subscript(integerRange: Range<Int>) -> String {
 		let start = advance(startIndex, integerRange.startIndex)
 			let end = advance(startIndex, integerRange.endIndex)
 			let range = start..<end
